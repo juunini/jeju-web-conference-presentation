6d454b18eb25a44042be9e988674a12435721ab9
Juunini (쥬니니) <juuni.ni.i@gmail.com>
2022-06-02 00:22:13 +0900

    Update avatar code

--- src/Avatar.tsx
+++ src/Avatar.tsx
@@ -5,14 +5,8 @@ import { Scene, Box, Plane, Camera } from '@belivvr/aframe-react';
 export default function App() {
   return (
     <Scene>
-      <Box
-        position={{ x: -1, y: 0.5, z: -3 }}
-        rotation={{ x: 0, y: 45, z: 0 }}
-        color="blue"
-      />
-
       <Plane
-        position={{ x: -1, y: 0.5, z: -16 }}
+        position={{ x: 0, y: 0, z: -10 }}
         scale={{ x: 10, y: 10, z: 10 }}
         mirror
       />
